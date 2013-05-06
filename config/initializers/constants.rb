2cb2e5f5fd97ec640562607045625533b9577d96
Junya Wako <j.wako@j-q.co.jp>
2013-05-06 16:41:43 +0900

    Fix secret key

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,11 @@
+unless ENV[AWS_SECRET_KEY].nil?
+  Amazon::Ecs.options = {
+    :associate_tag => 'jay1980-22',
+    :AWS_access_key_id => 'AKIAJMVEB42CHS7XAUNA',       
+    :AWS_secret_key => ENV[AWS_SECRET_KEY]
+  }
+end
+  
 ROOT_BROWSE_NODE_ID = 2275256051
 
 ROOT_STORE = 'KindleStore'
