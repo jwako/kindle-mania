d9c9df01f3bec9d8b1891a56236853ba257e719b
Junya Wako <j.wako@j-q.co.jp>
2013-05-06 16:23:36 +0900

    Adds book images

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,23 +3,13 @@ class PublicController < ApplicationController
     # res = Amazon::Ecs.browse_node_lookup(ROOT_BROWSE_NODE_ID, :response_group => 'TopSellers', :country => 'jp')
     # @res = (res.doc/"TopItem").collect { |item| Amazon::Element.new(item).get_hash }
     # puts @res
-    browse_node_id = params[:bn].blank? ? ROOT_BROWSE_NODE_ID : params[:bn]
-    res = Amazon::Ecs.browse_node_lookup(browse_node_id, :response_group => 'BrowseNodeInfo', :country => 'jp')
+    @browse_node_id = params[:bn].blank? ? ROOT_BROWSE_NODE_ID : params[:bn]
+    res = Amazon::Ecs.browse_node_lookup(@browse_node_id, :response_group => 'BrowseNodeInfo', :country => 'jp')
     @categories = (res.doc/"BrowseNode").collect { |item| Amazon::Element.new(item).get_hash }
     
-    # res = Amazon::Ecs.item_search(
-    #   ROOT_BROWSE_NODE_ID, 
-    #   { :type => 'BrowseNode', :response_group => 'BrowseNodeInfo' })
-    # res.items.each do |item|
-    #   puts item
-    # end
-    
-    # res = Amazon::Ecs.item_search('ruby', 
-    #   :search_index => ROOT_STORE,
-    #   :country => 'jp')
-    # res.items.each do |item|
-    #   puts item
-    # end
-    
+    res = Amazon::Ecs.item_search(
+      @browse_node_id, 
+      { :type => 'BrowseNode', :country => 'jp', :response_group => 'Large' })
+    @items = res.items
   end
 end
