ea8a83a87ff8710ce62f51f31f39a66895955131
Junya Wako <j.wako@j-q.co.jp>
2013-05-10 18:21:07 +0900

    Change kindle-mania to book voyager

--- app/helpers/public_helper.rb
+++ app/helpers/public_helper.rb
@@ -1,2 +1,11 @@
 module PublicHelper
+  
+  def book_image(image_url, detail_page_url)
+    if image_url.blank?
+      link_to image_tag("no-image.jpg", :class => "thumbnail"), detail_page_url, :target => "_blank"
+    else
+      link_to image_tag(image_url, :class => "thumbnail"), detail_page_url, :target => "_blank"
+    end
+  end
+  
 end
