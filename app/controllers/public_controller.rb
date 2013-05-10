914cdc6b03b531ef9ef3005841111a45922a98c6
Junya Wako <j.wako@j-q.co.jp>
2013-05-10 16:38:33 +0900

    Add masanory and infinite scroll

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,13 +3,18 @@ class PublicController < ApplicationController
     # res = Amazon::Ecs.browse_node_lookup(ROOT_BROWSE_NODE_ID, :response_group => 'TopSellers', :country => 'jp')
     # @res = (res.doc/"TopItem").collect { |item| Amazon::Element.new(item).get_hash }
     # puts @res
+    @page = params[:page].blank? ? 1 : params[:page].to_i
     @browse_node_id = params[:bn].blank? ? ROOT_BROWSE_NODE_ID : params[:bn]
     res = Amazon::Ecs.browse_node_lookup(@browse_node_id, :response_group => 'BrowseNodeInfo', :country => 'jp')
     @categories = (res.doc/"BrowseNode").collect { |item| Amazon::Element.new(item).get_hash }
     
     res = Amazon::Ecs.item_search(
       @browse_node_id, 
-      { :type => 'BrowseNode', :country => 'jp', :response_group => 'Large' })
+      { :type => 'BrowseNode', 
+        :country => 'jp', 
+        :response_group => 'Medium',
+        :sort => 'daterank',
+        :item_page => @page })
     @items = res.items
   end
 end
