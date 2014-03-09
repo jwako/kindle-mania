bf7b7b1cda0990a9926cc030674f6990aaa3181d
Junya Wako <j.wako@j-q.co.jp>
2014-03-09 11:49:31 +0900

    Changes env

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,12 +1,12 @@
 unless ENV["AWS_SECRET_KEY"].nil?
   Amazon::Ecs.options = {
-    :associate_tag => 'jay1980-22',
-    :AWS_access_key_id => 'AKIAJMVEB42CHS7XAUNA',       
+    :associate_tag => 'kindlemania02-20',
+    :AWS_access_key_id => 'AKIAJI354QNUTRN5WM2A',       
     :AWS_secret_key => ENV["AWS_SECRET_KEY"]
   }
 end
   
-# ROOT_BROWSE_NODE_ID = 2275256051
-# ROOT_STORE = 'KindleStore'
-ROOT_BROWSE_NODE_ID = 465610
-ROOT_STORE = 'Books'
+ROOT_BROWSE_NODE_ID = 2275256051
+ROOT_STORE = 'KindleStore'
+# ROOT_BROWSE_NODE_ID = 465610
+# ROOT_STORE = 'Books'
